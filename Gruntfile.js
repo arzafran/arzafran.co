eede80c4ad27fa9229a1858d2b5e4a5466beeb2c
Franco Arza <arzafran@gmail.com>
2016-05-23 21:03:05 -0300

    Added Gruntfile and main.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,33 @@
-require('load-grunt-tasks')(grunt);
-
-grunt.initConfig({
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     sass: {
-        options: {
-            sourceMap: true
-        },
-        dist: {
-            files: {
-                'main.css': 'main.scss'
-            }
+      options: {
+        sourceMap: false, // true if you need it
+        outputStyle: 'compressed' // try expanded for debugging
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/screen.scss'
         }
+      }
+    },
+    autoprefixer:{
+      dist:{
+        files:{
+          'css/main.css':'css/main.css'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['**/*.{scss,sass}'],
+        tasks: ['sass:dist', 'autoprefixer']
+      }
     }
-});
-
-grunt.registerTask('default', ['sass']);
+  });
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.registerTask('build', ['sass:dist', 'autoprefixer']);
+};
